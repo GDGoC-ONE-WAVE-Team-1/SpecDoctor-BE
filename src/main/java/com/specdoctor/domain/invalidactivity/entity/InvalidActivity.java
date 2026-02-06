084d595744ab692af9ccba257e8b32cbc8bdac14
yechan-kim <60172300+yechan-kim@users.noreply.github.com>
2026-02-07 01:48:02 +0900

    feat: 의심 활동 엔티티 구현

---
+++ src/main/java/com/specdoctor/domain/invalidactivity/entity/InvalidActivity.java
@@ -0,0 +1,36 @@
+package com.specdoctor.domain.invalidactivity.entity;
+
+import com.specdoctor.global.common.entity.BaseEntity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.validation.constraints.NotNull;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+public class InvalidActivity extends BaseEntity {
+
+	@NotNull
+	@Column(length = 50)
+	private String name;
+
+	@Column(columnDefinition = "TEXT")
+	private String operationEntity;
+
+	@Column(columnDefinition = "TEXT")
+	private String finance;
+
+	@Column(columnDefinition = "TEXT")
+	private String financeOpen;
+
+	@Column(columnDefinition = "TEXT")
+	private String leaderSelection;
+}
