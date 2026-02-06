32bc3f9163ca2166a67cf33554e962e84d08ee7d
yechan-kim <60172300+yechan-kim@users.noreply.github.com>
2026-02-06 19:13:52 +0900

    Initial commit

---
+++ src/test/java/com/specdoctor/SpecDoctorBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.specdoctor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(classes = SpecDoctorBeApplicationTests.class)
+class SpecDoctorBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
